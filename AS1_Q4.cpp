053596fc3c4c1e07550f9682dd1c224f997714b6
Amisha Goyal <agoyal9_be24@thapar.edu>
2026-01-29 23:50:55 +0530

    Assignment1 q4

---
+++ AS1_Q4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr[] = {-2, -5, 6, -2, -3, 1, 5, -6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int currSum = arr[0];
+    int maxSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSum = max(maxSum, currSum);
+    }
+
+    cout << maxSum;
+    return 0;
+}
